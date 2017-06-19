748c538f7bae91473a0214b016199f3e9e6d6805
Ricardo Faria <ricardo.faria@outlook.com.br>
2017-06-19 07:48:54 -0300

    #2 Desativado handlebars, migraremos para um client externo

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,6 @@ import * as express from 'express';
 import * as logger from 'morgan';
 import * as bodyParser from 'body-parser';
 import MqttServer from './mqtt-server';
-import * as ExpressHandlebars from 'express-handlebars';
 
 // Creates and configures an ExpressJS web server.
 class App {
@@ -15,7 +14,6 @@ class App {
   constructor() {
     this.express = express();
     this.middleware();
-    this.templateEngine();
     this.routes();
     MqttServer.init();
   }
@@ -34,22 +32,12 @@ class App {
      * API endpoints */
     let router = express.Router();
     // placeholder route handler
-    /*router.get('/', (req, res, next) => {
+    router.get('/', (req, res, next) => {
       res.json({
         message: 'Hello World!'
       });
-    });*/
-    router.get('/', (req, res) => {
-      res.render('home');
     });
-    console.log(__dirname);
-    this.express.use('/scripts/bootstrap/', express.static(__dirname + '/../../node_modules/bootstrap/dist/'));
     this.express.use('/', router);
-  }
-
-  private templateEngine() {
-    this.express.engine('handlebars', ExpressHandlebars({defaultLayout: 'main'}));
-    this.express.set('view engine', 'handlebars');
   }
 
 }
